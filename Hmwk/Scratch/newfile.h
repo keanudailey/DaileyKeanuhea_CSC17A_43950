8a12476aa9822537cd2d4b9b44cec9cb819d79dc
Keanu Dailey <keanudailey@yahoo.com>
2015-03-25 13:48:40 -0700

    assign 3 up 1

---
+++ Hmwk/Scratch/newfile.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   newfile.h
+ * Author: rcc
+ *
+ * Created on March 25, 2015, 1:31 PM
+ */
+
+#ifndef ARRAY_H
+#define	ARRAY_H
+struct Array{
+   int *data;
+   int size; 
+};
+#endif	/* NEWFILE_H */
+
